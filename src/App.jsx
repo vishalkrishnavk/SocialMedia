aa1b25076618ce557daeb0efb0311d02e19c6b39
vishalkrishnavk <vishalkrishnauvp@gmail.com>
2024-12-05 10:30:55 +0530

    changed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import ProfileRightbar from './components/ProfileRightBar'
 import './App.css'
 
