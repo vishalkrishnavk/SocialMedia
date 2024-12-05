44744231e35849ada8fe7e99dc112df5a0e160c8
vishalkrishnavk <vishalkrishnauvp@gmail.com>
2024-12-05 10:27:36 +0530

    commited

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ProfileRightbar from './components/ProfileRightBar'
+import './App.css'
+
+function App() {
+  return <ProfileRightbar />
+}
+  
+export default App
---
+++ src/components/ProfileRightBar.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+
+/* import ads from "../Images/ads.jpg";
+import image1 from "../Images/image1.jpg";
+import image2 from "../Images/image2.jpg";
+import image3 from "../Images/image3.jpg";
+import image4 from "../Images/image4.jpg";
+import image5 from "../Images/image5.jpg";
+import addfriends from "../Images/add-user.png"; */
+export default function ProfileRightbar() {
+  return (
+    <div className='Profilerightbar'>
+        <div>
+            <h1>Hello</h1>
+        </div>
+        <div className='rightcontainer'>
+            <div className='adscontainer'>{/* 
+                <img src={ads} className='addimage' alt=''/> */}
+                <div>
+                    <p style={{textAlign:'start',marginLeft:'10px',marginTop:"-20px"}}>AD SECTION</p>
+                    <p style={{textAlign:'start',marginLeft:'10px',fontSize:'12px',marginTop:'-16px', color: 'red'}}>Space for Ad</p>
+                </div>
+            </div>
+            <div className='adscontainer'>
+                {/* <img src={image1} className='addimage' alt=''/> */}
+                <div>
+                    <p style={{textAlign:'start',marginLeft:'10px',marginTop:"-20px"}}>AD SECTION</p>
+                    <p style={{textAlign:'start',marginLeft:'10px',fontSize:'12px',marginTop:'-16px'}}>Space for Ad</p>
+                </div>
+            </div>
+           
+
+        </div>
+        {/* <div className='rightcontainer2'>
+            <h3  style={{marginLeft:"10px",textAlign:"start"}}>Suggested for you</h3>
+            <div style={{marginTop:"-10px"}}>
+                <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+                    <div style={{display:"flex",alignItems:"center"}}>
+                        <img src={image3} className='profileimage' alt=''/>
+                        <div>
+                            <p style={{marginLeft:"10px",textAlign:"start"}}>User1</p>
+                            <p style={{marginLeft:"10px",textAlign:"start",marginTop:"-16px",fontSize:"11px",color:"#aaa"}}>Suggested for you</p>
+                        </div>
+                        
+                    </div>
+                    <div style={{backgroundColor:"#white",padding:"8px",marginRight:"18px",borderRadius:"50%",cursor:"pointer"}}>
+                        <img src={addfriends} className='addfriend' alt=''/>
+                    </div>
+                </div>
+            </div>
+            <div style={{marginTop:"-10px"}}>
+                <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+                    <div style={{display:"flex",alignItems:"center"}}>
+                        <img src={image5} className='profileimage' alt=''/>
+                        <div>
+                            <p style={{marginLeft:"10px",textAlign:"start"}}>User3</p>
+                            <p style={{marginLeft:"10px",textAlign:"start",marginTop:"-16px",fontSize:"11px",color:"#aaa"}}>Suggested for you</p>
+                        </div>
+                        
+                    </div>
+                    <div style={{backgroundColor:"#white",padding:"8px",marginRight:"18px",borderRadius:"50%",cursor:"pointer"}}>
+                        <img src={addfriends} className='addfriend' alt=''/>
+                    </div>
+                </div>
+            </div>
+            <div style={{marginTop:"-10px"}}>
+                <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+                    <div style={{display:"flex",alignItems:"center"}}>
+                        <img src={image2} className='profileimage' alt=''/>
+                        <div>
+                            <p style={{marginLeft:"10px",textAlign:"start"}}>User3</p>
+                            <p style={{marginLeft:"10px",textAlign:"start",marginTop:"-16px",fontSize:"11px",color:"#aaa"}}>Suggested for you</p>
+                        </div>
+                        
+                    </div>
+                    <div style={{backgroundColor:"#white",padding:"8px",marginRight:"18px",borderRadius:"50%",cursor:"pointer"}}>
+                        <img src={addfriends} className='addfriend' alt=''/>
+                    </div>
+                </div>
+            </div>
+            <div style={{marginTop:"-10px"}}>
+                <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+                    <div style={{display:"flex",alignItems:"center"}}>
+                        <img src={image5} className='profileimage' alt=''/>
+                        <div>
+                            <p style={{marginLeft:"10px",textAlign:"start"}}>User4</p>
+                            <p style={{marginLeft:"10px",textAlign:"start",marginTop:"-16px",fontSize:"11px",color:"#aaa"}}>Suggested for you</p>
+                        </div>
+                        
+                    </div>
+                    <div style={{backgroundColor:"#white",padding:"8px",marginRight:"18px",borderRadius:"50%",cursor:"pointer"}}>
+                        <img src={addfriends} className='addfriend' alt=''/>
+                    </div>
+                </div>
+            </div>
+            <div style={{marginTop:"-10px"}}>
+                <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+                    <div style={{display:"flex",alignItems:"center"}}>
+                        <img src={image4} className='profileimage' alt=''/>
+                        <div>
+                            <p style={{marginLeft:"10px",textAlign:"start"}}>User5</p>
+                            <p style={{marginLeft:"10px",textAlign:"start",marginTop:"-16px",fontSize:"11px",color:"#aaa"}}>Suggested for you</p>
+                        </div>
+                        
+                    </div>
+                    <div style={{backgroundColor:"#white",padding:"8px",marginRight:"18px",borderRadius:"50%",cursor:"pointer"}}>
+                        <img src={addfriends} className='addfriend' alt=''/>
+                    </div>
+                </div>
+            </div>
+            <div style={{marginTop:"-10px"}}>
+                <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
+                    <div style={{display:"flex",alignItems:"center"}}>
+                        <img src={image5} className='profileimage' alt=''/>
+                        <div>
+                            <p style={{marginLeft:"10px",textAlign:"start"}}>User6</p>
+                            <p style={{marginLeft:"10px",textAlign:"start",marginTop:"-16px",fontSize:"11px",color:"#aaa"}}>Suggested for you</p>
+                        </div>
+                        
+                    </div>
+                    <div style={{backgroundColor:"#white",padding:"8px",marginRight:"18px",borderRadius:"50%",cursor:"pointer"}}>
+                        <img src={addfriends} className='addfriend' alt=''/>
+                    </div>
+                </div>
+            </div>
+        </div> */}
+    </div>
+  )
+}
