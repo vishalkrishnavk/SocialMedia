434bac86e4200204b93796e9eb269f4486e93c14
vishalkrishnavk <vishalkrishnauvp@gmail.com>
2024-12-05 10:38:28 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 
 import ProfileRightbar from './components/ProfileRightBar'
 import './App.css'
 
 function App() {
-  return <ProfileRightbar />
+  return <ProfileRightbar/>
 }
   
 export default App
